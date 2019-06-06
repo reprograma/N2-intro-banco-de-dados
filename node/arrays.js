5118a8aae594f9064d59c8f37c0157a4816d7a4f
Leticia Tiveron <leticiativeronbt@gmail.com>
2019-06-06 18:50:41 -0300

    Add arrays exercises

---
+++ node/arrays.js
@@ -0,0 +1,28 @@
+const comidas = [
+  'Batata frita',
+  'Hamburguer',
+  'Sorvete de chocolate',
+  'Saladinha delicia'
+]
+
+// Exercício: Selecione apenas a batatinha
+
+// Exercício: transforme a lista de comidas em comidas divertidas (adicione uma carinha feliz nelas)
+
+const comidas2 = [
+  { nome: 'Batata frita', vegano: true },
+  { nome: 'Hamburguer', vegano: false },
+  { nome: 'Sorvete de chocolate', vegano: false },
+  { nome: 'Saladinha delicia', vegano: true }
+]
+
+// Exercício: Selecione apenas as comidinhas veganas.
+
+const comidas3 = [
+  { nome: 'Batata frita', vegano: true, valor: 5.00 },
+  { nome: 'Hamburguer', vegano: false, valor: 18.00 },
+  { nome: 'Sorvete de chocolate', vegano: false, valor: 7.00 },
+  { nome: 'Saladinha delicia', vegano: true, valor: 12.00 }
+]
+
+// Exercício: Qual o custo total dessa refeição maneira?! 
