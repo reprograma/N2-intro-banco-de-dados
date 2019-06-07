0075b308020473f15b562e67d929bb8a8c510f85
Leticia Tiveron <leticiativeronbt@gmail.com>
2019-06-06 22:17:15 -0300

    Add responses to array methods

--- node/arrays.js
+++ node/arrays.js
@@ -6,8 +6,29 @@ const comidas = [
 ]
 
 // Exercício: Selecione apenas a batatinha
+// Jeito simples de fazer o filter:
+const batatinha = comidas.filter(comida => comida === 'Batata frita')
+console.log(batatinha)
+
+// Jeito mais complicado de fazer o filter:
+const batatinhaComplicada = comidas.filter(comida => {
+  if(comida === 'Batata frita') return true
+  else return false
+})
+
+
+const comida = 'teste'
+console.log(comida === 'Batata frita')
+
 
 // Exercício: transforme a lista de comidas em comidas divertidas (adicione uma carinha feliz nelas)
+const comidasFelizesEComplexas = comidas.map(comida => {
+  return comida + ' :D xD o/'
+})
+
+const comidasFelizes = comidas.map(comida => comida + ' :D xD o/')
+console.log(comidasFelizes)
+
 
 const comidas2 = [
   { nome: 'Batata frita', vegano: true },
@@ -17,6 +38,13 @@ const comidas2 = [
 ]
 
 // Exercício: Selecione apenas as comidinhas veganas.
+const comidasVeganasEComplexas = comidas2.filter(comida => {
+  return comida.vegano === true
+})
+
+const comidasVeganas = comidas2.filter(comida => comida.vegano === true)
+console.log(comidasVeganas)
+
 
 const comidas3 = [
   { nome: 'Batata frita', vegano: true, valor: 5.00 },
@@ -26,3 +54,19 @@ const comidas3 = [
 ]
 
 // Exercício: Qual o custo total dessa refeição maneira?! 
+
+let custoDaRefeicao = 0
+comidas3.forEach(comida => {
+  custoDaRefeicao = custoDaRefeicao + comida.valor
+})
+
+const custoDaRefeicaoComplexa = comidas3.reduce(
+  (acumulador, comida) => {
+    return acumulador + comida.valor
+  } , 0)
+
+console.log(custoDaRefeicaoComplexa)
+
+const sorteiaNumero = Math.floor(Math.random() * 36)
+
+console.log('*** sorteiaNumero:', sorteiaNumero)
